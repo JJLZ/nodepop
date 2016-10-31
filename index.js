532cd8b5baf7bfaf755271ff043edf74a9bafeec
JJLZ <JJLZ@MacBook-Pro-ESoft.local>
2016-10-31 18:46:36 -0500

    Add .gitignore

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
 
     response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    response.end('Hello world..');
 });
 
 server.listen(1337, '127.0.0.1');
 console.log('Servidor arrancado en http://127.0.0.1:1337');
-console.log('OK');
+console.log('Ready...');
