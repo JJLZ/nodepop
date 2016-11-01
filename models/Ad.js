b9e578d609ddc7f4bb93917e942271b451abf9e6
JJLZ <JJLZ@MacBook-Pro-ESoft.local>
2016-10-31 21:07:39 -0500

    Agregando imagenes y los modelos Ad.js y User.js

---
+++ models/Ad.js
@@ -0,0 +1,15 @@
+/**
+ * Created by JJLZ on 10/31/16.
+ */
+
+"use strict";
+
+var mongoose = require('mongoose');
+
+var anuncioSchema = mongoose.Schema({
+    nombre: String,
+    venta: Boolean,
+    precio: Number,
+    foto: String,
+    tags: [String]
+});
---
+++ models/User.js
@@ -0,0 +1,12 @@
+/**
+ * Created by JJLZ on 10/31/16.
+ */
+
+"use strict";
+
+var usuarioSchema = mongoose.Schema({
+    nombre: String,
+    email: String,
+    clave: String
+});
+
